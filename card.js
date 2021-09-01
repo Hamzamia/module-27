1a9b7daf3ffa87a02a353e7f2b6a414c967448c2
Hamzammia <mahmudul.web.developer@gmail.com>
2021-09-01 20:28:01 +0600

    add

--- card.js
+++ card.js
@@ -1,13 +1,54 @@
+// function updateCase(isIncreasing) {
+//     const caseInput = document.getElementById('case-number');
+//     const caseNumber = caseInput.value;
+//     // caseInput.value = parseInt(caseNumber) + 1
+//     if (isIncreasing == true) {
+//         caseInput.value = parseInt(caseNumber) + 1;
+//     }
+//     else {
+//         casenumber.value = parseInt(casenumberText) - 1;
+
+//     }
+// }
+
+
+
+// document.getElementById('input-plus').addEventListener('click', function () {
+//     updateCase(true);
+
+// })
+
+// // document.getElementById('input-minus').addEventListener('click', function () {
+// //     //console.log("minus button clicked");
+// //     const caseInput = document.getElementById('case-number');
+// //     const caseInputText = caseInput.value;
+// //     caseInput.value = parseInt(caseInputText) - 1;
+// // })
+
+// document.getElementById('input-minus').addEventListener('click', function () {
+
+//     // const casenumber = document.getElementById('case-number');
+//     // const casenumberText = casenumber.value;
+//     updateCase(false);
+//     // casenumber.value = parseInt(casenumberText) - 1;
+
+// })
+
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseInput.value = parseInt(caseNumber) + 1;
+    }
+    else {
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+
+}
+
 document.getElementById('input-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number');
-    const caseInputText = caseInput.value;
-    caseInput.value = parseInt(caseInputText) + 1;
-
-})
-
+    updateCaseNumber(true);
+});
 document.getElementById('input-minus').addEventListener('click', function () {
-    //console.log("minus button clicked");
-    const caseInput = document.getElementById('case-number');
-    const caseInputText = caseInput.value;
-    caseInput.value = parseInt(caseInputText) - 1;
+    updateCaseNumber(false);
 })
