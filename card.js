38afc5145da4c575e72e4988007bfe5d8990eb47
Hamzammia <mahmudul.web.developer@gmail.com>
2021-09-01 17:21:34 +0600

    add

--- card.js
+++ card.js
@@ -5,6 +5,9 @@ document.getElementById('input-plus').addEventListener('click', function () {
 
 })
 
-document.getElementById().addEventListener('click', function () {
-    console.log("minus button clicked");
+document.getElementById('input-minus').addEventListener('click', function () {
+    //console.log("minus button clicked");
+    const caseInput = document.getElementById('case-number');
+    const caseInputText = caseInput.value;
+    caseInput.value = parseInt(caseInputText) - 1;
 })
